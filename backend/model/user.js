6560577e86ac7eee26cd73e8341acf88b4c86738
Natalie <ncha0086@student.monash.edu>
2024-03-02 13:17:39 +1100

    created initial user and request model

--- backend/model/user.js
+++ backend/model/user.js
@@ -8,10 +8,38 @@ const mongoose = require("mongoose");
 
 
 /**
- * Defines the schema for the Category model.
+ * Defines the schema for the User model.
  */
 const userSchema = mongoose.Schema({
-	
+	Id:{},
+	firstName: {
+		type: String,
+		required: true
+	},
+	lastName: {
+		type :String,
+		required: true
+	},
+	degree: {
+		type: String,
+		required: true
+	},
+	units: [{
+		type: String,
+		required: true
+	}],
+	bio: {
+		type: String
+	},
+	password: {
+		type: String
+	},
+	contact:[{
+		type: String
+	}]
+
+
 });
 
-module.exports = mongoose.model("User", userSchema);
+
+
